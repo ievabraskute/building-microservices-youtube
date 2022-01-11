851df1b40d58003b766775c2dff19cb75db1922b
ibraske <i.braskute@gmail.com>
2022-01-11 14:46:51 +0200

    add episode 1 code

--- product-api/main.go
+++ product-api/main.go
@@ -8,31 +8,21 @@ import (
 )
 
 func main() {
-	// reqeusts to the path /goodbye with be handled by this function
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World")
+		d, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			http.Error(rw, "Oops", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(rw, "Hello %s\n", d)
+	})
+
 	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
 		log.Println("Goodbye World")
 	})
 
-	// any other request will be handled by this function
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Running Hello Handler")
-
-		// read the body
-		b, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Println("Error reading body", err)
-
-			http.Error(rw, "Unable to read request body", http.StatusBadRequest)
-			return
-		}
-
-		// write the response
-		fmt.Fprintf(rw, "Hello %s", b)
-	})
-
-	// Listen for connections on all ip addresses (0.0.0.0)
-	// port 9090
-	log.Println("Starting Server")
-	err := http.ListenAndServe(":9090", nil)
-	log.Fatal(err)
+	http.ListenAndServe(":9090", nil)
 }
